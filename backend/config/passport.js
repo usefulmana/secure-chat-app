abfa780adb822cee8600ef268857d10257885ad6
Alex Nguyen <alex.nguyen.3141@gmail.com>
2020-08-05 12:30:09 +1000

    Finished creating User model

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,2 +1,17 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+
+let opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET
+};
+
+module.exports = function(passport){
+    passport.serializeUser((user, done) => done(null, { id: user.id, _socket: user._socket }));
+
+    passport.deserializeUser((user, done) => {
+        
+    });
+
+};
--- backend/server.js
+++ backend/server.js
@@ -20,17 +20,25 @@ const helmet = require('helmet');
 
 // ** Socket IO **
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 // ** Routes **
 
 
 // ** Middleware **
 
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(expressValidator());
+app.use(cors());
 
 // ** Route Definitions **
 
 
 // ** Run server **
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
     console.log(`[INFO] Server is running on port ${process.env.PORT}`)
 });

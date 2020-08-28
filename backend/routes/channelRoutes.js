1e19e57da0f40b48630d101ae0deaa9b2116e205
s3664619 <s3664619@student.rmit.edu.au>
2020-08-28 20:52:53 +1000

    Change Channel name

--- backend/routes/channelRoutes.js
+++ backend/routes/channelRoutes.js
@@ -5,21 +5,28 @@ const { Server } = require("../models/Server");
 const { Channel } = require("../models/Channel");
 const { User } = require("../models/User");
 
-router.post('/create', passport.authenticate("jwt", { session: false }), async( req, res) =>{
+router.post('/create', passport.authenticate("jwt", { session: false }), async (req, res) => {
 
     const { channelName, serverId } = req.body;
 
-    const server =  await Server.findById(serverId)
-    .then( s => {
-        const newChannel = Channel({
-            name: channelName,
-            server: serverId
-        }).save().then(channel => {
-            res.status(200).json(channel);
-        });
-    }).catch(err => res.status(404).send({message: "Server Not Found"}));
+    const server = await Server.findById(serverId)
+        .then(s => {
+            const newChannel = Channel({
+                name: channelName,
+                server: serverId
+            }).save().then(channel => {
+                res.status(200).json(channel);
+            });
+        }).catch(err => res.status(404).send({ message: "Server Not Found" }));
 });
 
+// Edit Channel Name
+router.put("/name", passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+        const channel = await Channel.findById(req.channel.id).select('name');
+        channel.name = req.body.name;
+        channel.save().then(u => res.status(200).send(u)).catch(err => console.log(err));
+    })
 
 
 

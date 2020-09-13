8589bf73f91c09f712ee6bc80ba42a10877f8cc1
Alex Nguyen <alex.nguyen.3141@gmail.com>
2020-09-13 16:57:08 +0700

    Added get chat routes

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,1 +1,26 @@
 
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const { Message } = require("../models/Message");
+
+
+// Get Chat Messages
+router.get("/:id",passport.authenticate("jwt", { session: false }), async (req, res) => {
+    const channelId = req.params.id;
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const options = {
+        page: page,
+        limit: limit,
+        sort: {created_at: -1}
+    }
+    
+    await Message.paginate({channel: channelId}, options, function(error, pageCount, paginatedResults) {    
+        res.status(200).json(pageCount);
+    });
+    
+})
+
+module.exports = router;

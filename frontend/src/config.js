1cae95b725a342ac483996730863354e31025215
Alex Nguyen <alex.nguyen.3141@gmail.com>
2020-09-18 17:45:59 +0700

    Finished containerizing the frontend and deployed to GCP Cloud Run

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'http://localhost:5000'
-export const BASE_URL = 'https://pp1-chat.herokuapp.com'
+export const BASE_URL = 'https://chattr-backend-o5uuteegxq-ts.a.run.app'
 
 
 // export const BASE_URL = BASE_URL;

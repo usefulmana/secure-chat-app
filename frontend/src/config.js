1fb2a52cfc00c6fd2d0fe5350fcf454af2bdaa57
Alex Nguyen <alex.nguyen.3141@gmail.com>
2020-09-24 10:46:59 +0700

    Moved Base url to .env

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,4 @@
 // export const BASE_URL = 'http://localhost:5000'
-require('dotenv').config()
-console.log("process.env :", process.env)
-// export const BASE_URL = "https://chattr-backend-o5uuteegxq-ts.a.run.app"
 
-// export const BASE_URL = "https://chattr-backend-o5uuteegxq-ts.a.run.app";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = process.env.REACT_APP_BASE_URL | "http://localhost:5000";
+

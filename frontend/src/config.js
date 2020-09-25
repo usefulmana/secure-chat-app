f3cd9f6380196f7364bb433d1aae9256f6fffa89
Alex Nguyen <alex.nguyen.3141@gmail.com>
2020-09-25 17:02:44 +0700

    Safety Commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
+
 // export const BASE_URL = 'http://localhost:5000'
 
-export const BASE_URL = process.env.REACT_APP_BASE_URL | "http://localhost:5000";
-
+export const BASE_URL = "https://chattr-backend-o5uuteegxq-ts.a.run.app";

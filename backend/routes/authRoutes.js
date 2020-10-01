11ae04b5c15276c2e2b57523642d40557b21f24b
Alex Nguyen <alex.nguyen.3141@gmail.com>
2020-10-01 09:48:53 +0700

    Fixed social login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -144,7 +144,16 @@ router.get('/google', passport.authenticate('google'));
 router.get('/facebook', passport.authenticate('facebook', {scope: 'email'}));
 
 // ** Social Auth Callbacks **
-router.get('/google/redirect', passport.authenticate('google', {failureRedirect: '/login'}), socialAuthActions.google);
-router.get('/facebook/redirect', passport.authenticate('facebook'), socialAuthActions.facebook);
-
+router.get('/google/redirect', passport.authenticate('google', {failureRedirect: '/login'}), (req, res) => {
+    const token = jwt.sign(req.user.details.toObject(), process.env.JWT_SECRET, {
+        expiresIn: 18000
+    });
+    res.redirect(`${process.env.CLIENT_URL}?token=${token}`)
+});
+router.get('/facebook/redirect', passport.authenticate('facebook'), (req, res) => {
+    const token = jwt.sign(req.user.details.toObject(), process.env.JWT_SECRET, {
+        expiresIn: 18000
+    });
+    res.redirect(`${process.env.CLIENT_URL}?token=${token}`)
+});
 module.exports = router;

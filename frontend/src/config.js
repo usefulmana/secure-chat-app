d35cac8f2f445ba51ead6410463d78fcfaf8bf7e
sam <besamly2018@gmail.com>
2020-10-05 22:04:07 +0900

    fixed token expired

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,4 @@
-// export const BASE_URL = 'http://localhost:5000'
-export const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
+const BACKEND_URL="https://chattr-backend-o5uuteegxq-ts.a.run.app/"
+// const BACKEND_URL='http://localhost:5000'
+
+export const BASE_URL = process.env.REACT_APP_BASE_URL || BACKEND_URL;

d480a454dac610f8d6ada2f98c8062d12b1e458e
sam <besamly2018@gmail.com>
2020-10-12 18:21:47 +0900

    update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // const BACKEND_URL="https://chattr-backend-o5uuteegxq-ts.a.run.app/"
-const BACKEND_URL='http://localhost:5000'
+const BACKEND_URL='http://54.252.209.37:5000'
 
 export const BASE_URL = process.env.REACT_APP_BASE_URL || BACKEND_URL;

2da0fcc7ac8e40b6b2c297019721dff4b6ca6275
sam <besamly2018@gmail.com>
2020-10-14 14:43:23 +0900

    cleaned up code

--- backend/server.js
+++ backend/server.js
@@ -94,9 +94,7 @@ app.use((req, res, next) => {
 });
 
 // ** SocketIO
-
 let clients = [];
-
 
 // ** For live chatting
 const rooms = {};
@@ -124,60 +122,6 @@ io.on("connection", (socket) => {
         socket.broadcast.to(msg.channelId).emit(`${msg.channelId}-notification`, payload);
       });
   });
-
-  // Sam's comment : this is not used
-  // Normal Chat Message in a channel
-  // socket.on("private-channel-message", async (msg) => {
-  //   const newMessage = Message({
-  //     user: msg.userId,
-  //     channel: msg.channelId,
-  //     message: msg.message,
-  //   })
-  //     .save()
-  //     .then((msg) => {
-  //       payload = { type: "message", payload: msg };
-
-  //       io.to(msg.channelId).emit("update", payload);
-  //     });
-  // });
-
-  // Sam's comment : this is not used
-  // One to one direct message
-  // socket.on("private-chat-message", async (msg) => {
-  //   const newPrivateMessage = PrivateMessage({
-  //     message: msg.message,
-  //     from: msg.from,
-  //     to: msg.to,
-  //   }).save();
-
-  //   // Find which socket to send TO
-  //   action = {
-  //     type: "private-message",
-  //     payload: {
-  //       from: message.from,
-  //       to: message.to,
-  //       msg: message.msg,
-  //       user: message.from.toLowerCase(),
-  //     },
-  //   };
-  //   clients.find((client) => {
-  //     if (client.userId === to[0].user_id) {
-  //       io.to(client.id).emit("update", action);
-  //     }
-  //   });
-
-  //   // Find which socket to
-  //   action = {
-  //     type: 'private-message',
-  //     payload: { from: message.from, to: message.to, msg: message.msg, user: message.to.toLowerCase() }
-  //   };
-  //   clients.find(client => {
-  //     if (client.userId === from[0].user_id) {
-  //       io.to(client.id).emit('update', action);
-  //     }
-  //   });
-
-  // });
 
   // When a rooms sign in, he/she sends over his/her userId
   // Add to a list of clients userId to identify socket.id
@@ -232,11 +176,7 @@ io.on("connection", (socket) => {
   socket.on("join room", ({ roomID, username }) => {
     console.log("socket id: ", socket.id, " joined room event received roomId: ", roomID)
     if (rooms[roomID]) {
-      // const length = rooms[roomID].length;
-      // if (length === 4) {
-      //   socket.emit("room full");
-      //   return;
-      // }
+      
       rooms[roomID].push({ socketId: socket.id, username });
     } else {
       rooms[roomID] = [{ socketId: socket.id, username }];
@@ -272,127 +212,17 @@ io.on("connection", (socket) => {
       room = room.filter(user => user.socketId !== socket.id);
       rooms[roomID] = room;
 
-      console.log("room after filter: ", room)
       room.map((user) => {
         io.to(user.socketId).emit('user left', { peerID: socket.id });
       })
-      console.log("room.length : ", room.length)
 
       if (room.length === 0) {
         console.log("emit call finisehd event : room : ", room)
-        // console.log("emit call finisehd event")
         io.to(roomID).emit('call finished');
-        // socket.broadcast.to(roomID).emit('call finished');
-
       }
     }
   });
 
-  // socket.on('disconnect-live-chat', () => {
-  //   const roomID = socketToRoom[socket.id];
-  //   let room = rooms[roomID];
-  //   if (room) {
-  //     room = room.filter(id => id !== socket.id);
-  //     rooms[roomID] = room;
-
-  //     room.map((id) => {
-  //       io.to(id).emit('user left', { peerId: socket.id });
-  //     })
-
-  //     console.log("room.length : ", room.length)
-  //     if (room.length === 0) {
-  //       console.log("emit call finisehd event")
-  //       io.to(room).emit('call finished');
-  //     }
-  //   }
-  // });
-
-  /*========================
-  Anh's live chat code
-   ========================*/
-
-  // Signaling for webRTC
-  //   socket.on("voice-signal", (data) => {
-  //     const toId = data.userId;
-  //     data.userId = sessionUserId;
-  //     let action = { type: "voice-signal ", payload: data };
-  //     clients.find((client) => {
-  //       if (client.userId === toId) {
-  //         io.to(client.id).emit("update", action);
-  //       }
-  //     });
-  //   });
-
-  //   // Emit list of connections when user joins voice on specific channel
-  //   socket.on("user-join-voice", (data) => {
-  //     // Join room with channel id
-  //     socket.join(data.channelId);
-  //     // Get socket ids for rooms in that channel
-  //     const socketIdsInChannel = Object.keys(
-  //       io.sockets.in(data.channelId).sockets
-  //     );
-  //     const userIdsInChannel = [];
-  //     // Find user ids in channel
-  //     clients.forEach((client) => {
-  //       socketIdsInChannel.forEach((socketId) => {
-  //         if (client.id === socketId)
-  //           userIdsInChannel.push({
-  //             userId: client.userId,
-  //             userName: client.userName,
-  //           });
-  //       });
-  //     });
-
-  //     // Emit to everyone on this channel, the socketid of new user, and list of clients in room
-  //     let action = {
-  //       type: "user-join-voice",
-  //       payload: { userId: data.userId, clients: userIdsInChannel },
-  //     };
-  //     io.to(data.channelId).emit("update", action);
-  //   });
-
-  //   // Emit list of connections when user leaves voice on specific channel
-  //   socket.on("user-leave-voice", (data) => {
-  //     // Leave channel
-  //     socket.leave(data.channelId);
-  //     const userIdsInChannel = [];
-  //     // Emit to everyone in that room that user left voice
-  //     io.in(data.channelId).clients((error, socketClients) => {
-  //       socketClients.forEach((socketClientId) => {
-  //         // Find user ids in channel
-  //         clients.forEach((client) => {
-  //           if (client.id === socketClientId) {
-  //             userIdsInChannel.push({
-  //               userId: client.userId,
-  //               userName: client.userName,
-  //             });
-  //           }
-  //         });
-  //       });
-
-  //       let action = {
-  //         type: "user-leave-voice",
-  //         payload: { userId: data.userId, clients: userIdsInChannel },
-  //       };
-  //       io.to(data.channelId).emit("update", action);
-  //     });
-  //   });
-
-  //   // On disconnect, remove from the client list
-  //   socket.on("disconnect", () => {
-  //     clients.find((client, i) => {
-  //       if (client.userId === sessionUserId) {
-  //         // Emit to all connected rooms that this user left (disconnects all voice peering calls with him)
-  //         let action = {
-  //           type: "user-leave-voice",
-  //           payload: { userId: client.userId },
-  //         };
-  //         socket.emit("update", action);
-  //         // Remove from global socket client list
-  //         return clients.splice(i, 1);
-  //       }
-  //     });
-  //   });
 });
 
 // ** Run server **
